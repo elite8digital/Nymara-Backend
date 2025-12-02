f90f1a2b83213e6999c5d7bf68e679a21b3978f6
Sai Subham Jena <jenasaisubham8@gmail.com>
2025-12-02 22:07:39 +0530

    Update server listening address to '0.0.0.0'

--- app.js
+++ app.js
@@ -97,4 +97,9 @@ app.use("/api/contact", contactRoutes);
 
   
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`hi Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`hi Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`hi Server running on port ${PORT}`);
+});
+
+
