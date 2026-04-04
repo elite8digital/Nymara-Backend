f0b53a82f307369e29cc81aa81d060c364a22e05
Sai Subham Jena <jenasaisubham8@gmail.com>
2026-04-04 12:20:47 +0530

    Create Wishlist.js

---
+++ models/Wishlist.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const wishlistSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, unique: true },
+    items: [{ type: mongoose.Schema.Types.ObjectId, ref: "Ornament" }],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Wishlist", wishlistSchema);
